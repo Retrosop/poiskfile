43720deed7534e24946852404dd423b4b51454cf
retros <glagolev-jar@yandex.ru>
2022-07-26 01:41:07 +1000

    Add access to MariaDB 10.3

---
+++ Poiskfile/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Poiskfile
+{
+ 
+
+        public class DataContext : DbContext
+        {
+            public DataContext() : base() { }
+            public DbSet<MeteoData> MeteoDatas { get; set; }
+            
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+            optionsBuilder.UseMySql("server=localhost;user=root;password=root;database=meteonasa;", new MySqlServerVersion(new Version(10, 3, 0)));
+        }
+        }
+   
+
+}
---
+++ Poiskfile/MeteData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Poiskfile
+{
+    public class MeteoData {
+
+        public int Id { get; set; }
+        public int station { get; set; }
+        public string date { get; set; }
+        public int wnd1 { get; set; }
+        public int wnd2 { get; set; }
+        public string wnd3 { get; set; }
+        public int wnd4 { get; set; }
+        public int wnd5 { get; set; }
+        public int tmp1 { get; set; }
+        public string tmp2 { get; set; }
+        public int dew1 { get; set; }
+        public string dew2 { get; set; }
+        public string aa11 { get; set; }
+        public string aa12 { get; set; }
+        public string aa13 { get; set; }
+        public string aa14 { get; set; }
+    }
+
+}
